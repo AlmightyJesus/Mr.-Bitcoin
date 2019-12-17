7a4ae4b99f8fc030546b235a473ab43e700d292f
Ori Shemla <orish1001@gmail.com>
2019-12-17 18:09:19 +0200

    Updates

---
+++ precache-manifest.c45c22ce9c479bb0ddcbd033177ec509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c10ef6de140d61a57f9405ed5b3b15f",
+    "url": "/Mr.-Bitcoin/index.html"
+  },
+  {
+    "revision": "6376d7d20a78a96b9ec1",
+    "url": "/Mr.-Bitcoin/static/css/main.aed26e6d.chunk.css"
+  },
+  {
+    "revision": "f95f6bede1384d6d246e",
+    "url": "/Mr.-Bitcoin/static/js/2.ec6ff297.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Mr.-Bitcoin/static/js/2.ec6ff297.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6376d7d20a78a96b9ec1",
+    "url": "/Mr.-Bitcoin/static/js/main.c72c65e0.chunk.js"
+  },
+  {
+    "revision": "df53c4874fbe4f7fc801",
+    "url": "/Mr.-Bitcoin/static/js/runtime-main.948cee6d.js"
+  }
+]);
